20299aa5a4cb11daf516513e8f9e8c69a33a9216
Aadarsha Man Shrestha <a.shrestha998@gmail.com>
2020-12-08 12:36:03 +0545

    Question 3

---
+++ MessageApp/app/src/main/java/com/aadarsha/messageapp/ConstraintDesignActivity.kt
@@ -0,0 +1,11 @@
+package com.aadarsha.messageapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstraintDesignActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_design)
+    }
+}
